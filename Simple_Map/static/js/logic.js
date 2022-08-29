68f9eef15380b5e2f220216a028b2a9b7a309630
sh-4 <105808695+sh-4@users.noreply.github.com>
2022-08-29 16:03:26 -0400

    adding files for simple leaflet map

---
+++ Simple_Map/static/js/logic.js
@@ -0,0 +1,12 @@
+// Create the map object with a center and zoom level.
+// the center is at approximate center of the US
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// create the tile layer that will be the background of the map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// add the 'streets' tile layer to the map.
+streets.addTo(map);
