e057edf70c504f5b9f2e000277ab196c61cb9f83
sh-4 <105808695+sh-4@users.noreply.github.com>
2022-08-30 14:30:47 -0400

    adding multiple city circle markers

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,19 +1,26 @@
 // Create the map object with a center and zoom level.
-// // the center is at approximate center of the US
-// let map = L.map('mapid').setView([40.7, -94.5], 14);
+// the center is at approximate center of the US
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// // the center is at approximate center of LA, CA
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+// Get data from cities.js
+let cityData = cities;
 
-// add a circle marker to the map for Los Angeles, California.
- L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: "#ffffa1"
- }).addTo(map);
+// Loop through the cities array and create one marker for each city.
+// add radius dependent upon city population/100000 for scale
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup(
+        "<h2>" + city.city + ", " 
+        + city.state + "</h2> <hr> <h3>Population: " 
+        + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 // create the tile layer that will be the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
