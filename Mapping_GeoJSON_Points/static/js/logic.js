42b38eaa6e03e7467a4ea8d9ca2d78f563377cec
sh-4 <105808695+sh-4@users.noreply.github.com>
2022-08-31 13:48:23 -0400

    adding GeoJSON mapping points

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,55 @@
+// Create the map object with a center and zoom level.
+// // the center is at approximate center of the US
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// the center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+                // note - coordinate order: long/lat
+]};
+
+// // Grabbing our GeoJSON data. - pointToLayer
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+//       return L.marker(latlng)
+//       .bindPopup("<h2>" + feature.properties.name + "</h2><hr><h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+//     }
+//   }).addTo(map);
+
+// Grabbing our GeoJSON data. - onEachFeature
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h4> Airport Name: " + feature.properties.name + "</h4><hr><h4>Airport Code: " + feature.properties.faa + "</h4>");
+    }
+  }).addTo(map);
+
+// create the tile layer that will be the background of the map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// add the 'streets' tile layer to the map.
+streets.addTo(map);
